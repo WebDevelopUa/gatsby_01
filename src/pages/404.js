d385eeaf3ece50870beb03f38a66aa1cdb5529d4
webdevelopua <js.develop.com@gmail.com>
2021-04-19 01:40:31 +0300

    Fix 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 
-export default () => <Layout>
-  <h1>Error Page</h1>
-  <p>Go to: <Link to={`/`}>Homepage</Link></p>
-</Layout>
+export default function ErrorPage() {
+  return <Layout>
+    <h1>Error Page</h1>
+    <p>Go to: <Link to={`/`}>Homepage</Link></p>
+  </Layout>
+}
