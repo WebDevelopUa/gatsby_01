74e9bd0859a848f440cc3d49dc695559f6542caa
webdevelopua <js.develop.com@gmail.com>
2021-04-19 01:46:27 +0300

    Add Recipes Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,9 @@ const Navbar = () => {
         <li>
           <Link to={`/about`}>About</Link>
         </li>
+        <li>
+          <Link to={`/recipes`}>Recipes</Link>
+        </li>
       </ul>
     </nav>
   )
---
+++ src/pages/recipes.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Recipes() {
+  return <Layout>
+    <h1>Recipes Page</h1>
+  </Layout>
+}
