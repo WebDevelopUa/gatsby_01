767477e0803696d70c6ebbe637217e50416519ed
webdevelopua <js.develop.com@gmail.com>
2021-04-21 00:42:30 +0300

    Add Error Page styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return <footer className="page-footer">
     <p>&copy; {new Date().getFullYear()}
-      <span> Simple <a href="https://www.gatsbyjs.com" target="_blank">Gatsby 3.0</a> Website</span>
+      <span> Simple <a href="https://www.gatsbyjs.com" target="_blank" rel="noreferrer">Gatsby 3.0</a> Website</span>
     </p>
   </footer>
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 
-export default function ErrorPage() {
+export default function Error() {
   return <Layout>
-    <h1>Error Page</h1>
-    <p>Go to: <Link to={`/`}>Homepage</Link></p>
+    <main className="error-page">
+      <section>
+        <h1>404</h1>
+        <h2>Error Page</h2>
+        <h3>Page not found</h3>
+        <h4>Go to: <Link to={`/`}>Homepage</Link></h4>
+      </section>
+    </main>
+
   </Layout>
 }
