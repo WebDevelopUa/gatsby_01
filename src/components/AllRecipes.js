4a6bc4065ea2bccb624d27673d892cb2f8be818a
webdevelopua <js.develop.com@gmail.com>
2021-04-25 02:01:34 +0300

    Add allContentfulRecipe query to AllRecipes component

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,11 +1,37 @@
 import React from "react"
 import TagsList from "./TagsList"
 import RecipesList from "./RecipesList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: {fields: title, order: ASC}) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 export default function AllRecipes() {
+  const { allContentfulRecipe: { nodes: recipes } } = useStaticQuery(query)
+
+  // const data = useStaticQuery(query)
+  // const recipes = data.allContentfulRecipe.nodes
+  // console.log(recipes)
+
   return <div>
     <h4>all recipes</h4>
-    <TagsList />
-    <RecipesList />
+    <TagsList recipes={recipes} />
+    <RecipesList recipes={recipes} />
   </div>
 }
