306e8653e3424ad6e32f9cdeab1eb54020dc1d43
webdevelopua <js.develop.com@gmail.com>
2021-04-26 01:55:18 +0300

    Add Tags List

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,7 +1,20 @@
 import React from "react"
+import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
-export default function TagsList() {
-  return <div>
-    <h4>tags list</h4>
+export default function TagsList({ recipes }) {
+  const newTags = setupTags(recipes)
+
+  return <div className="tags-container">
+    <h4>tags list (recipes)</h4>
+    <div className="tags-list">
+      {newTags.map((tag, index) => {
+        const [text, value] = tag
+
+        return <Link to={`/${text}`} key={index}>
+          {text}({value})
+        </Link>
+      })}
+    </div>
   </div>
 }
---
+++ src/utils/setupTags.js
@@ -0,0 +1,21 @@
+export default function setupTags(recipes) {
+  const allTags = {}
+
+  recipes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      if (allTags[tag]) {
+        allTags[tag] = allTags[tag] + 1
+      } else {
+        allTags[tag] = 1
+      }
+    })
+  })
+
+  return Object.entries(allTags)
+    .sort((a, b) => {
+        const [firstTag] = a
+        const [secondTag] = b
+        return firstTag.localeCompare(secondTag)
+      }
+    )
+}
