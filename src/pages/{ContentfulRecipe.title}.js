8d5e8f63b78157cc667b94c433a513b05d0e3f8b
webdevelopua <js.develop.com@gmail.com>
2021-04-27 22:57:10 +0300

    Add GraphQL query to dynamic Contentful Recipe pages and render title

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,7 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { getImage } from "gatsby-plugin-image"
 
-export default function RecipeTemplate(props) {
-  return <div>
-    <h2>{props.params.title}</h2>
-  </div>
+export const query = graphql`
+ query getSingleRecipe($title: String) {
+    contentfulRecipe(title: {eq: $title}) {
+      title
+      cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
+export default function RecipeTemplate({ data }) {
+  const { title, cookTime, content, prepTime, servings, description: { description }, image } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
+
+  return <main className="page">
+    <div className="recipe-page">
+      <h2>{title}</h2>
+    </div>
+  </main>
 }
