8f7c9b72c457d3eb62e77e8b538df4828ac69019
webdevelopua <js.develop.com@gmail.com>
2021-04-27 23:33:47 +0300

    Render all content on detail Recipe pages

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,6 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { getImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../components/Layout"
+import { BsClock, BsClockHistory, BsPeople } from "react-icons/all"
 
 export const query = graphql`
  query getSingleRecipe($title: String) {
@@ -29,9 +31,84 @@ export default function RecipeTemplate({ data }) {
   const pathToImage = getImage(image)
   const { tags, instructions, ingredients, tools } = content
 
-  return <main className="page">
-    <div className="recipe-page">
-      <h2>{title}</h2>
-    </div>
-  </main>
+  return <Layout>
+    <main className="page">
+      <div className="recipe-page">
+
+        <section className="recipe-hero">
+          <GatsbyImage alt={title} image={pathToImage} className="about-img" />
+          <article className="recipe-info">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div className="recipe-icons">
+              <article>
+                <BsClock />
+                <h5>prep time</h5>
+                <p>{prepTime} min.</p>
+              </article>
+
+              <article>
+                <BsClockHistory />
+                <h5>cook time</h5>
+                <p>{cookTime} min.</p>
+              </article>
+
+              <article>
+                <BsPeople />
+                <h5>serving</h5>
+                <p>{servings}</p>
+              </article>
+
+            </div>
+            <p className="recipe-tags">
+              Tags: {tags.map(
+              (tag, index) => {
+                return <Link to={`/${tag}`} key={index}>{tag}</Link>
+              }
+            )}
+            </p>
+          </article>
+        </section>
+
+        <section className="recipe-content">
+
+          <article>
+            <h4>instructions</h4>
+            {instructions.map(
+              (item, index) => {
+                return <div key={index} className="single-instruction">
+                  <header>
+                    <p>step {index + 1}</p>
+                    <div />
+                  </header>
+                  <p>{item}</p>
+                </div>
+              }
+            )}
+          </article>
+
+          <article className="second-column">
+            <div>
+              <h4>ingredients</h4>
+              {ingredients.map(
+                (item, index) => {
+                  return <p key={index} className="single-ingredient">{item}</p>
+                }
+              )}
+            </div>
+            <div>
+              <h4>tools</h4>
+              {tools.map(
+                (item, index) => {
+                  return <p key={index} className="single-tool">{item}</p>
+                }
+              )}
+            </div>
+          </article>
+
+        </section>
+
+      </div>
+    </main>
+  </Layout>
 }
