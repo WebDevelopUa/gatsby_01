6a1cef32f7e5f65d206cb5ad7598058f83378c45
webdevelopua <js.develop.com@gmail.com>
2021-04-29 17:27:59 +0300

    Render Tag pages using tag template with gatsby-node.js

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,7 +1,42 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import RecipesList from "../components/RecipesList"
 
-export default function TagTemplate() {
-  return <div>
-    <h2> tag template page</h2>
-  </div>
+export const query = graphql`
+query GetRecipeByTag($tag: String) {
+  allContentfulRecipe(
+    sort: {fields: title, order: ASC}
+    filter: {content: {tags: {eq: $tag}}}
+  ) {
+    edges {
+      node {
+        id
+      }
+    }
+    nodes {
+      title
+      id
+      cookTime
+      prepTime
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+}
+`
+
+export default function TagTemplate({ data, pageContext }) {
+  const recipes = data.allContentfulRecipe.nodes
+
+  return <Layout>
+    <main className="page">
+      <h2>{pageContext.tag}</h2>
+      <div className="tag-recipes">
+        <RecipesList recipes={recipes} />
+      </div>
+    </main>
+
+  </Layout>
 }
